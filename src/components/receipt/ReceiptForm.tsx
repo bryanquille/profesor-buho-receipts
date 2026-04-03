7d869f1a59ee1336c303455209b1bbab315d8201
Bryan Quille <103023942+bryanquille@users.noreply.github.com>
2026-04-03 02:48:10 -0500

    add validation and form handling to ReceiptForm; integrate @hookform/resolvers and update schemas

--- src/components/receipt/ReceiptForm.tsx
+++ src/components/receipt/ReceiptForm.tsx
@@ -1,9 +1,51 @@
+import { useFieldArray, useForm } from "react-hook-form"
 import { AddIcon, RemoveIcon } from "../ui/Icons"
+import { ReceiptSchema, type ReceiptData, mappedModalityOptions } from "../../lib/schemas"
+import { zodResolver } from "@hookform/resolvers/zod"
+
+const initialValue = {
+  customerName: '',
+  studentName: '',
+  teacherName: '',
+  subjectOrService: '',
+  modality: 'online' as const,
+  pricePerHour: 0,
+  items: [{
+    dateOfClasses: '',
+    hoursOfClasses: 0,
+    subtotal: 0
+  }],
+  notesOrObservations: ''
+}
 
 function ReceiptForm() {
+  const {
+    register,
+    control,
+    handleSubmit,
+    reset,
+    // watch,
+    formState: { errors }
+  } = useForm<ReceiptData>({
+    resolver: zodResolver(ReceiptSchema),
+    defaultValues: initialValue
+  })
+
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: 'items'
+  })
+
+  const onSubmit = (data: ReceiptData) => {
+    console.log(data)
+    reset(initialValue)
+  }
+
+
   return (
     <form
-      className="w-11/12 mx-auto p-3 flex flex-col gap-4 border-2 rounded-2xl bg-gray-300"
+      className="w-11/12 mx-auto p-3 flex flex-col gap-4 border-2 rounded-2xl bg-gray-300 dark:bg-primary"
+      onSubmit={handleSubmit(onSubmit)}
     >
       <div>
         <h2 className="font-semibold text-lg">Detalles del cliente</h2>
@@ -16,16 +58,46 @@ function ReceiptForm() {
           type="text"
           id="customerName"
           placeholder="Ej. John Doe"
+          {...register('customerName')}
         />
+        {
+          errors.customerName
+          && <span className="text-red-500 text-xs" >
+            {errors.customerName.message}
+          </span>
+        }
       </label>
 
       <label htmlFor="studentName">
-        Nombre de estudiante
+        Nombre del estudiante
         <input
           type="text"
           id="studentName"
           placeholder="Ej. Junior Doe"
+          {...register('studentName')}
         />
+        {
+          errors.studentName
+          && <span className="text-red-500 text-xs" >
+            {errors.studentName.message}
+          </span>
+        }
+      </label>
+
+      <label htmlFor="teacherName">
+        Nombre del profesor
+        <input
+          type="text"
+          id="teacherName"
+          placeholder="Ej. Dr. Smith"
+          {...register('teacherName')}
+        />
+        {
+          errors.teacherName
+          && <span className="text-red-500 text-xs" >
+            {errors.teacherName.message}
+          </span>
+        }
       </label>
 
       <label htmlFor="subjectOrService">
@@ -34,7 +106,43 @@ function ReceiptForm() {
           type="text"
           id="subjectOrService"
           placeholder="Ej. Matemáticas"
+          {...register('subjectOrService')}
         />
+        {
+          errors.subjectOrService
+          && <span className="text-red-500 text-xs" >
+            {errors.subjectOrService.message}
+          </span>
+        }
+      </label>
+      
+      <label
+        htmlFor="modality"
+        className="flex flex-col justify-center items-start"
+      >
+        <span>Modalidad</span>
+        <select
+          id="modality"
+          {...register('modality')}
+        >
+          {
+            Object.entries(mappedModalityOptions).map(([key, value]) => (
+              <option
+                value={key}
+                key={key}
+                className="dark:text-primary"
+              >
+                {value}
+              </option>
+            ))
+          }
+        </select>
+        {
+          errors.modality
+          && <span className="text-red-500 text-xs" >
+            {errors.modality.message}
+          </span>
+        }
       </label>
 
       <label htmlFor="pricePerHour">
@@ -43,41 +151,107 @@ function ReceiptForm() {
           type="number"
           id="pricePerHour"
           placeholder="Ej. $10.00"
+          {...register('pricePerHour', { valueAsNumber: true })}
         />
+        {
+          errors.pricePerHour
+          && <span className="text-red-500 text-xs" >
+            {errors.pricePerHour.message}
+          </span>
+        }
       </label>
 
       <div className="w-full">
         <h3 className="mb-0.5">Clases/Entregas</h3>
-        <div className="w-full flex justify-between items-center gap-2">
-          <input
-            type="date"
-            className="w-1/4"
-          />
-          <input
-            type="number"
-            placeholder="Horas"
-            className="w-1/4"
-          />
-          <input
-            type="number"
-            placeholder="$ Precio"
-            className="w-1/4"
-          />
-          <button
-            type="button"
-          >
-            <RemoveIcon />
-          </button>
-        </div>
+
+        {
+          fields.map((field, index) => (
+            <div key={field.id}>
+              <div
+                className="w-full flex justify-between items-end gap-2"
+              >
+                <label htmlFor={`dateOfClasses-${index}`}>
+                  <span className="text-sm">Fecha</span>
+                  <input
+                    type="date"
+                    className="w-1/4"
+                    id={`dateOfClasses-${index}`}
+                    {...register(`items.${index}.dateOfClasses` as const)}
+                  />
+                </label>
+                <label htmlFor={`hoursOfClasses-${index}`}>
+                  <span className="text-sm">Horas</span>
+                  <input
+                    type="number"
+                    id={`hoursOfClasses-${index}`}
+                    placeholder="Horas"
+                    className="w-1/4"
+                    {...register(`items.${index}.hoursOfClasses` as const, { valueAsNumber: true })}
+                  />
+                </label>
+                <label htmlFor={`subtotal-${index}`}>
+                  <span className="text-sm">Subtotal</span>
+                  <input
+                    type="number"
+                    id={`subtotal-${index}`}
+                    placeholder="$ Subtotal"
+                    className="w-1/4"
+                    {...register(`items.${index}.subtotal` as const, { valueAsNumber: true })}
+                  />
+                </label>
+                <button
+                  type="button"
+                  disabled={fields.length === 1}
+                  onClick={() => remove(index)}
+                  className="cursor-pointer py-2 disabled:opacity-50 disabled:cursor-not-allowed"
+                >
+                  <RemoveIcon />
+                </button>
+              </div>
+              <div className="mt-1.5 mb-2 flex flex-col justify-center items-start gap-1">
+                {
+                  errors.items?.[index]?.dateOfClasses
+                  && <span
+                    className="text-red-500 text-xs"
+                  >
+                    {errors.items[index].dateOfClasses?.message}
+                  </span>
+                }
+                {
+                  errors.items?.[index]?.hoursOfClasses
+                  && <span
+                    className="text-red-500 text-xs"
+                  >
+                    {errors.items[index].hoursOfClasses?.message}
+                  </span>
+                }
+                {
+                  errors.items?.[index]?.subtotal
+                  && <span
+                    className="text-red-500 text-xs"
+                  >
+                    {errors.items[index].subtotal?.message}
+                  </span>
+                }
+              </div>
+            </div>
+          ))
+        }
       </div>
 
       <button
         type="button"
-        className="cursor-pointer flex items-center gap-2"
+        className="cursor-pointer w-fit -mt-3 p-2 flex items-center gap-2 border-2 border-primary rounded-2xl transition-all ease-in-out duration-200 hover:border-secondary hover:bg-secondary"
+        onClick={() => append({
+          dateOfClasses: '',
+          hoursOfClasses: 0,
+          subtotal: 0,
+        })}
       >
         <AddIcon />
         <span>Añadir nueva entrada</span>
       </button>
+
 
       <label htmlFor="notesOrObservations">
         Notas/Observaciones
@@ -85,12 +259,19 @@ function ReceiptForm() {
           id="notesOrObservations"
           rows={5}
           className="resize-none"
+          {...register('notesOrObservations')}
         ></textarea>
+        {
+          errors.notesOrObservations
+          && <span className="text-red-500 text-xs" >
+            {errors.notesOrObservations.message}
+          </span>
+        }
       </label>
 
       <button
         type="submit"
-        className="p-3 rounded-2xl font-semibold text-lg bg-secondary"
+        className="cursor-pointer p-3 rounded-2xl font-semibold text-lg bg-secondary hover:bg-amber-400 dark:text-primary"
       >
         Generar
       </button>
--- src/lib/schemas.ts
+++ src/lib/schemas.ts
@@ -1,22 +1,63 @@
 import { z } from 'zod';
 
+const modalityOptions = ["online", "in-person", "hybrid"] as const
+
+export type ModalityOptionsTypes = (typeof modalityOptions)[number]
+
+export const mappedModalityOptions: {[key in ModalityOptionsTypes]: string} = {
+  "online": "Virtual",
+  "in-person": "Presencial",
+  "hybrid": "Híbrida"
+}
+
 // Sub-schema for individual receipt items (classes or sessions)
 const ReceiptItemSchema = z.object({
-  classOfDate: z.string().min(1, "La fecha es requerida"),
-  hoursOfClasses: z.number().positive("Las horas deben ser mayores a 0"),
-  subtotal: z.number()
+  dateOfClasses: z
+    .string()
+    .min(1, "La fecha es requerida."),
+
+  hoursOfClasses: z
+    .coerce.number({ message: "Las horas de clase son requeridas." })
+    .positive("Las horas deben ser mayores a 0."),
+
+  subtotal: z
+    .coerce.number({ message: "El valor subtotal es requerido." })
+    .positive("El valor subtotal debe ser mayor a 0."),
 });
 
-// main schema for the receipt form
+// Main schema for the receipt form
 export const ReceiptSchema = z.object({
-  customerName: z.string().min(2, "El nombre del cliente es muy corto"),
-  studentName: z.string().min(2, "El nombre del estudiante es muy corto"),
-  subjectOrService: z.string().min(2, "Debes especificar la materia o servicio"),
-  pricePerHour: z.number().positive("El precio debe ser mayor a 0"),
-  items: z.array(ReceiptItemSchema).min(1, "Debes agregar al menos una clase o sesión"),
+  customerName: z
+    .string()
+    .min(1, "El nombre del cliente es requerido.")
+    .min(2, "El nombre del cliente es muy corto."),
+
+  studentName: z.string().optional(),
+
+  teacherName: z
+    .string()
+    .min(1, "El nombre del profesor es requerido.")
+    .min(2, "El nombre del profesor es muy corto."),
+
+  subjectOrService: z
+    .string()
+    .min(1, "Debes especificar la materia o servicio.")
+    .min(2, "La materio o servicio es demasiado corto."),
+
+  modality: z
+    .enum(modalityOptions, { message: 'Debes escoger una modalidad.' }),
+
+  pricePerHour: z
+    .coerce.number({ message: "El precio por hora es requerido." })
+    .positive("El precio debe ser mayor a 0"),
+
+  items: z
+    .array(ReceiptItemSchema)
+    .min(1, "Debes agregar al menos una clase o sesión"),
+
   notesOrObservations: z.string().optional(),
 });
 
 // Types inferred from the schemas for use in the application
-export type ReceiptData = z.infer<typeof ReceiptSchema>;
+export type ReceiptData = z.input<typeof ReceiptSchema>;
 export type ReceiptItem = z.infer<typeof ReceiptItemSchema>;
