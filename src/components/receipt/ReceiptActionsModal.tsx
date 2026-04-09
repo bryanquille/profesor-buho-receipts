a99dd7e12755b5d3a003d77fd626dd89182b965b
Bryan Quille <103023942+bryanquille@users.noreply.github.com>
2026-04-08 20:18:33 -0500

    add loader for generating receipts

--- src/components/receipt/ReceiptActionsModal.tsx
+++ src/components/receipt/ReceiptActionsModal.tsx
@@ -9,18 +9,15 @@ interface ReceiptActionsModalPropsTypes {
 }
 
 function ReceiptActionsModal({ isOpen, isLoading, onClose, image }: ReceiptActionsModalPropsTypes) {
-  if (!isOpen || !image) return null
-  // TODO: Implement the loader correctly, right now it just shows a loader if the image is loading, but it should show the loader while the image is being generated, and then show the image when it's ready.
-  if (isLoading) return (<Loader />)
-
   const handleDownload = () => {
     const link = document.createElement('a');
     link.download = `Recibo-${Date.now()}.png`;
-    link.href = image;
+    if (image) link.href = image;
     link.click();
   }
 
   const handleShare = async () => {
+    if (!image) return;
     const blob = await (await fetch(image)).blob();
     const file = new File([blob], 'recibo.png', { type: blob.type });
 
@@ -35,47 +32,56 @@ function ReceiptActionsModal({ isOpen, isLoading, onClose, image }: ReceiptActio
   }
 
   return (
+    isOpen &&
     <div className="fixed inset-0 grid place-items-center bg-mist-900/90">
       <section
         className="w-11/12 max-w-125 p-4 flex flex-col justify-center items-center gap-3 border-2 border-gray-900 rounded-xl bg-gray-200"
       >
-        <header className="relative p-3 w-full dark:text-slate-950">
-          <h2 className="font-semibold text-xl text-center">Vista previa y acciones</h2>
-          <button
-            type="button"
-            onClick={onClose}
-            className="cursor-pointer absolute top-2 right-0 p-2 rounded-full hover:bg-gray-200 transition-colors duration-150 ease-in md:top-3 md:right-3"
-          >
-            <CloseIcon />
-          </button>
-        </header>
-        <main>
-          {image && (
-            <img
-              src={image}
-              alt="Vista previa del recibo"
-              className="max-h-[60vh] object-contain"
-            />
-          )}
-        </main>
-        <footer className="mt-auto mb-5 p-3 flex justify-evenly items-center gap-6 border-2 border-gray-500 rounded-xl bg-gray-300 text-slate-800 dark:bg-primary dark:text-primary-neutral">
-          <button
-            type="button"
-            onClick={handleDownload}
-            className="cursor-pointer p-3 grid place-items-center gap-1.5 rounded-xl  hover:outline-2 hover:text-slate-950 hover:inset-shadow-xs hover:inset-shadow-slate-400 dark:hover:text-secondary dark:hover:outline-secondary transition-all duration-100 ease-in"
-          >
-            <DownloadIcon />
-            <span className="text-sm">Descargar</span>
-          </button>
-          <button
-            type="button"
-            onClick={handleShare}
-            className="cursor-pointer p-3 grid place-items-center gap-1.5 rounded-xl hover:outline-2 hover:text-slate-950 hover:inset-shadow-xs hover:inset-shadow-slate-400 dark:hover:text-secondary dark:hover:outline-secondary transition-all duration-100 ease-in"
-          >
-            <ShareIcon />
-            <span className="text-sm">Compartir</span>
-          </button>
-        </footer>
+        {isLoading &&
+          <div className="grid place-items-center gap-2">
+            <Loader />
+            <p className="text-2xl">Generando Recibo...</p>
+          </div>
+        }
+        {(image && !isLoading) &&
+          <>
+            <header className="relative p-3 w-full dark:text-slate-950">
+              <h2 className="font-semibold text-xl text-center">Vista previa y acciones</h2>
+              <button
+                type="button"
+                onClick={onClose}
+                className="cursor-pointer absolute top-2 right-0 p-2 rounded-full hover:bg-gray-200 transition-colors duration-150 ease-in md:top-3 md:right-3"
+              >
+                <CloseIcon />
+              </button>
+            </header>
+            <main>
+              <img
+                src={image}
+                alt="Vista previa del recibo"
+                className="max-h-[60vh] object-contain"
+              />
+            </main>
+            <footer className="mt-auto mb-5 p-3 flex justify-evenly items-center gap-6 border-2 border-gray-500 rounded-xl bg-gray-300 text-slate-800 dark:bg-primary dark:text-primary-neutral">
+              <button
+                type="button"
+                onClick={handleDownload}
+                className="cursor-pointer p-3 grid place-items-center gap-1.5 rounded-xl  hover:outline-2 hover:text-slate-950 hover:inset-shadow-xs hover:inset-shadow-slate-400 dark:hover:text-secondary dark:hover:outline-secondary transition-all duration-100 ease-in"
+              >
+                <DownloadIcon />
+                <span className="text-sm">Descargar</span>
+              </button>
+              <button
+                type="button"
+                onClick={handleShare}
+                className="cursor-pointer p-3 grid place-items-center gap-1.5 rounded-xl hover:outline-2 hover:text-slate-950 hover:inset-shadow-xs hover:inset-shadow-slate-400 dark:hover:text-secondary dark:hover:outline-secondary transition-all duration-100 ease-in"
+              >
+                <ShareIcon />
+                <span className="text-sm">Compartir</span>
+              </button>
+            </footer>
+          </>
+        }
       </section>
     </div>
   )
--- src/hooks/useGenerateReceipt.ts
+++ src/hooks/useGenerateReceipt.ts
@@ -2,24 +2,25 @@ import { toPng } from "html-to-image"
 import { useRef, useState } from "react"
 
 export const useGenerateReceipt = () => {
-  const [isModalReceiptOpen, setIsModalReceiptOpen] = useState(true)
+  const [isModalReceiptOpen, setIsModalReceiptOpen] = useState(false)
   const [isLoading, setIsLoading] = useState(false)
   const [receiptImage, setReceiptImage] = useState<string | null>(null)
   const previewRef = useRef<HTMLElement | null>(null)
 
   const handleGenerate = async () => {
     if (previewRef.current === null) return
+    setIsModalReceiptOpen(true)
     setIsLoading(true)
 
     const dataUrl = await toPng(previewRef.current, { cacheBust: true })
     setReceiptImage(dataUrl)
 
     setIsLoading(false)
-    setIsModalReceiptOpen(true)
   }
 
   const onClose = () => {
     setIsModalReceiptOpen(false)
+    setIsLoading(false)
   }
 
   return {
